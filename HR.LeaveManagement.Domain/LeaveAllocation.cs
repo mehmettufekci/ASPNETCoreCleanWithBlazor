88d0062a53234252be82f6632af265918761d88d
Mehmet TUFEKCI <mtufekci1446@hotmail.com>
2023-12-04 00:49:04 +0300

    Add project files.

---
+++ HR.LeaveManagement.Domain/LeaveAllocation.cs
@@ -0,0 +1,11 @@
+﻿namespace HR.LeaveManagement.Domain;
+
+public class LeaveAllocation
+{
+    public int Id { get; set; }
+    public int NumberOfDays { get; set; }
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public int Period { get; set; }
+
+}
---
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain;
+
+public class LeaveType
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
