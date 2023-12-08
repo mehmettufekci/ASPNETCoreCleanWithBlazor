d7ddedd53408c879ba72689b44abd309370c603c
Mehmet TUFEKCI <mtufekci1446@hotmail.com>
2023-12-09 01:41:50 +0300

    Logger implementation added

--- HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
+++ HR.LeaveManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Contracts.Logging;
 using HR.LeaveManagement.Application.Models.Email;
 using HR.LeaveManagement.Infrastructure.EmailService;
+using HR.LeaveManagement.Infrastructure.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -12,7 +14,7 @@ namespace HR.LeaveManagement.Infrastructure
         {
             services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
             services.AddTransient<IEmailSender, EmailSender>();
-
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
             return services;
         }
     }
