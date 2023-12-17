ab4252662df1ca6312bd1405535a032ae7f0d33f
Mehmet TUFEKCI <mtufekci1446@hotmail.com>
2023-12-17 23:24:54 +0300

    Leave Allocation added

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocations/LeaveAllocationDto.cs
@@ -0,0 +1,20 @@
+﻿using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations
+{
+    public class LeaveAllocationDto
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        
+        //For DTOS Use DTO Objects. If you need domain object to use make sure this object have DTO and a mapping for it.
+        public LeaveTypeDto LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public int Period { get; set; }
+    }
+}
